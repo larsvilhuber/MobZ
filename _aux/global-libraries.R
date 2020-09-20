5c526b38749d8a8637b6443628ef5864f171192b
Lars Vilhuber <lars@vilhuber.com>
2020-09-20 13:53:57 -0400

    Further documentation

--- _aux/global-libraries.R
+++ _aux/global-libraries.R
@@ -3,7 +3,7 @@
 ####################################
 
 
-global.libraries <- c("config","DT","tibble","dplyr","knitr")
+global.libraries <- c("config","DT","tibble","dplyr","knitr","stringr")
 
 
 results <- sapply(as.list(global.libraries), pkgTest)
