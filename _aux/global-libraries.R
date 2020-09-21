1849e4f69f2c90e869bbe1ae2b1b6cfae5d82979
Lars Vilhuber <lars@vilhuber.com>
2020-09-20 21:04:26 -0400

    Latest update to README and data citations

--- _aux/global-libraries.R
+++ _aux/global-libraries.R
@@ -3,7 +3,7 @@
 ####################################
 
 
-global.libraries <- c("config","DT","tibble","dplyr","knitr","stringr")
+global.libraries <- c("config","DT","tibble","dplyr","knitr","stringr","tidyr","readxl","readr")
 
 
 results <- sapply(as.list(global.libraries), pkgTest)
